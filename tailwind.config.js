42b82f91adb676abf6b3131ff9dad11f3b5e0bab
Gustavo <gustavo.ribeiro.fernandes1@gmail.com>
2022-07-30 18:46:37 -0300

    Bug fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,7 +26,7 @@ module.exports = {
         },
       },
       boxShadow: {
-        'button': 'hsl(26, 100%, 90%) 0px 12px 30px 5px;'
+        'button': 'hsl(26, 100%, 90%) 0px 12px 30px 5px'
       }
     },
   },
